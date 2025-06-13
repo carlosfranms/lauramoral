04d8ab333c40a94646fddaff0a2284806d072505
carlosfranms <104785731+carlosfranms@users.noreply.github.com>
2025-06-14 00:54:02 +0100

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Página cargada correctamente");
+
+  // Puedes añadir más funcionalidades JS aquí si lo necesitas
+});
