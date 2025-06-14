1a7433e224e25c78271d0d220d55955febef5776
carlosfranms <104785731+carlosfranms@users.noreply.github.com>
2025-06-14 12:25:10 +0100

    Delete files directory

--- files/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Página cargada correctamente");
-
-  // Puedes añadir más funcionalidades JS aquí si lo necesitas
-});
